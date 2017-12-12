20fe5bfea87a56610e55cf0e0942051aadd83e2d
Mariano Mariano <marianofheller@gmail.com>
2017-12-12 16:10:12 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Map from './Map';
 import './App.css';
 
+const KEY_IP_LOC = '02c1559982a189';
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      initialLocation: undefined
+    }
+  }
+
+  componentDidMount() {
+    this.getCurrentPosition();
+  }
+
+  getCurrentPosition() {
+    axios.get('http://ipinfo.io', {
+      params: {
+        token: KEY_IP_LOC
+      }
+    })
+    .then( (res) => {
+      try {
+        var loc = res.data.loc.split(",").map(Number);
+      } catch(e) {
+        console.log("Unable to parse ip location response");
+        throw e;
+      }
+      this.setState({
+        initialLocation: loc
+      });
+    })
+    .catch( (err) => {
+      console.log(err);
+    })
+  }
+
   render() {
+    const { initialLocation } = this.state;
     return (
       <div className="App">
-        <Map />
+        <Map initialLocation={initialLocation}/>
       </div>
     );
   }
--- src/Map.js
+++ src/Map.js
@@ -11,8 +11,10 @@ export default class Map extends Component {
     }
 
     createMap() {
+        var { initialLocation } = this.props;
+        initialLocation = initialLocation || [34.6000,-58.4500];
         
-        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+        var mymap = L.map('mapid').setView(initialLocation, 13);
         L.tileLayer('https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}', {
             maxZoom: 20,
             attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
