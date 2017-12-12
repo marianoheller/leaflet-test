31d398e6336f5b1e55131cd6521b52c02d2181d9
Mariano Mariano <marianofheller@gmail.com>
2017-12-12 15:28:09 -0300

    init

--- src/Map.js
+++ src/Map.js
@@ -12,13 +12,16 @@ export default class Map extends Component {
 
     createMap() {
         
-        const mymap = L.map('mapid').setView([51.505, -0.09], 13);
-         L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-            maxZoom: 18,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+        L.tileLayer('https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}', {
+            maxZoom: 20,
+            attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(mymap);
-/*         var map = new L.Map("mapid", {center: [37.8, -96.9], zoom: 3})
-        .addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")); */
+
+        var popup = L.popup().setContent('<p>Hello world!<br />This is a nice popup.</p>'); 
+        mymap.on('contextmenu',function(e){
+            popup.setLatLng(e.latlng).openOn(mymap);
+        });
     }
 
     render() {
